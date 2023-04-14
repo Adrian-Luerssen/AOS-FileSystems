eec5f2a12d704abe58b6b0b37a6a4735a849b730
Adrian-Luerssen <91116739+Adrian-Luerssen@users.noreply.github.com>
2023-04-14 11:51:49 +0200

    identifying FAT systems

--- FAT.c
+++ FAT.c
@@ -5,5 +5,8 @@
 #include "FAT.h"
 
 bool isFat(int fd){
-    return fd == 0;
+    lseek(fd, BS_FILSYSTYPE_OFFSET, SEEK_SET);
+    char buffer[BS_FILSYSTYPE_SIZE];
+    read(fd, buffer, BS_FILSYSTYPE_SIZE);
+    return strcmp(buffer, FAT12_TYPE_STRING) == 0 || strcmp(buffer, FAT16_TYPE_STRING) == 0 || strcmp(buffer, FAT_TYPE_STRING) == 0;
 }
--- FAT.h
+++ FAT.h
@@ -5,6 +5,12 @@
 #ifndef AOS_FILESYSTEMS_FAT_H
 #define AOS_FILESYSTEMS_FAT_H
 #include <stdbool.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include "FAT_INFO.h"
 bool isFat(int fd);
 
 #endif //AOS_FILESYSTEMS_FAT_H
